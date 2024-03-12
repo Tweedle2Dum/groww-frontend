38268a4e459c153c3a8cbea776015c46b7dac425
Tweedle2Dum <utkarsh.work1405@gmail.com>
2024-03-13 00:40:33 +0530

    fix. button focus

--- frontend/Components/UI/Button/Button.tsx
+++ frontend/Components/UI/Button/Button.tsx
@@ -8,7 +8,7 @@ function Button(props: Props) {
   return (
     <button
       type="button"
-      className=" w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      className="  w-full text-white bg-blue-700 hover:bg-blue-800 focus:outline-none  font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       {props.content}
     </button>
